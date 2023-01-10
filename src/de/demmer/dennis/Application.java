3fc781ad99b276f820c012445b450980a6254e12
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-01-10 10:09:54 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,11 +1,22 @@
 package de.demmer.dennis;
 
+import java.util.Arrays;
 
 public class Application {
 	
 	public static void main(String[] args) {
 		
-	
+		int[] intArr = new int[5];
+		
+		intArr[0] = 9;
+		intArr[1] = 8;
+		intArr[2] = 1;
+		intArr[3] = 3;
+		intArr[4] = 8;
+
+		Arrays.sort(intArr);
+		
+		System.out.println(Arrays.toString(intArr));
 		
 		
 
